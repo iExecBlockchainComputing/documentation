668173443a94039fcb3539fc835003a923599c8a
Robin Le Caignec <72495599+Le-Caignec@users.noreply.github.com>
2025-08-08 10:10:38 +0200

    Update .vitepress/sidebar.ts
    Co-authored-by: Zied Guesmi <26070035+zguesmi@users.noreply.github.com>

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -56,7 +56,7 @@ export function getSidebar() {
             link: '/overview/tooling-and-explorers/builder-dashboard',
           },
           {
-            text: 'Bridge',
+            text: 'iExec RLC Bridge',
             link: '/overview/tooling-and-explorers/bridge',
           },
           {
