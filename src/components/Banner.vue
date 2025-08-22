3b3a72249d8776abe2e9461b2523b1840ebda496
ErwanDecoster <wanerd2@gmail.com>
2025-08-22 16:23:16 +0200

    feat: enhance Banner component styling with CSS variables and gradients

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-gradient-to-r from-[#fcd15a] to-[#ffad4d] rounded-[6px] px-8 pb-4 text-gray-800 max-w-3xl mx-auto mb-6">
+  <div
+    class="relative z-0 mx-auto mb-6 max-w-3xl rounded-2xl bg-gradient-to-b from-[var(--c-border)] to-transparent px-8 pb-4 text-[var(--vp-c-text-1)] *:relative *:z-20 after:absolute after:inset-px after:z-10 after:rounded-[15px] after:bg-gradient-to-r after:from-[var(--c-bg-gradient-from)] after:to-[var(--c-bg-gradient-to)]"
+  >
     <slot />
   </div>
 </template>
