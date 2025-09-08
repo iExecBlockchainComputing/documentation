604f43fc4ce90afb6a88da5cd84eadfe5ee82a99
Mathis Debuire <68806646+64ix@users.noreply.github.com>
2025-09-08 11:37:21 +0200

    fix: mise à jour des liens et rename de la page "iApp" pour  "Privacy iApp"

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -65,7 +65,7 @@ export function getSidebar() {
           },
           {
             text: 'Privacy iApp',
-            link: '/get-started/overview/iapp',
+            link: '/get-started/overview/privacy-iapp',
           },
           {
             text: 'Workerpool',
