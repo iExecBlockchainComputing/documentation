a6767f8270248c6875a95da74a51d4f771b9a79c
Le-Caignec <robinlecaignec@yahoo.fr>
2026-03-25 09:51:00 +0100

    feat: add GTM

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,7 +4,6 @@ import DefaultTheme from 'vitepress/theme';
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client';
 import Layout from './Layout.vue';
 import type { EnhanceAppContext } from 'vitepress';
-import googleAnalytics from 'vitepress-plugin-google-analytics';
 import 'virtual:group-icons.css';
 import '@shikijs/vitepress-twoslash/style.css';
 import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query';
@@ -39,10 +38,6 @@ export default {
     app.use(WagmiPlugin, { config: wagmiAdapter.wagmiConfig });
 
     app.component('ChainSelector', ChainSelector);
-
-    googleAnalytics({
-      id: 'GTM-P7KSD4T',
-    });
 
     if (typeof window !== 'undefined') {
       // Ensure dataLayer exists
